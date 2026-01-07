6edd970e9988daf284a987f3baeba17056ec9265
emilio-atlas <emilk93@protonmail.com>
2026-01-07 00:00:51 -0800

    Remove wallet connection from pumpfun bundler - Remove WalletConnection component and WalletContext provider - Revert App.jsx and main.jsx to remove wallet adapter integration - Remove wallet adapter CSS styles - Wallet connection belongs in Nodematrix-v2, not pumpfun bundler

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { WalletContextProvider } from './contexts/WalletContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <WalletContextProvider>
-      <App />
-    </WalletContextProvider>
+    <App />
   </React.StrictMode>,
 )
 
